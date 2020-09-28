28eb28c4403a7b7bee125a7fc077e8227a3f7055
achrafamil <achraf.amil@fabernovel.com>
2020-09-28 08:53:58 +0200

    Create dynamic feature module

---
+++ foofeature/src/main/java/com/fabernovel/bugreproduction/dynamic_navigation/foofeature/FooActivity.kt
@@ -0,0 +1,11 @@
+package com.fabernovel.bugreproduction.dynamic_navigation.foofeature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FooActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foo)
+    }
+}
